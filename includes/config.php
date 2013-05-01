898f3a957529af7b048c77dafbdae61d2d08e048
Sathia@iMac <sathia.musso@gmail.com>
2013-05-01 18:26:28 +0200

    Magari addiamoli i files :)

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,15 @@
 <?php
 	/* File di configurazione del sito */
-	
+
 	/* Inizio configurazione generale */
 	// Prefisso della tabella nel database MySQL
 	$_CONFIG['table_suffix']="av_";
 	/* Fine configurazione generale */
-	
+
 	/* Inizio parametri del database */
 	$db_host = "localhost";
-	$db_user = "root";
-	$db_password = "oyju7T3I";
+	$db_user = "borghesia";
+	$db_password = "tianming";
 	$db_name = "avideo";
 	/* Fine parametri del database */
 ?>
