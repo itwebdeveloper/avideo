147172724bdd770019aa1127255a5ea2ccef5363
Sathia@iMac <sathia.musso@gmail.com>
2013-05-01 18:19:27 +0200

    Added add_random.php

--- includes/funzioni.php
+++ includes/funzioni.php
@@ -1,6 +1,6 @@
 <?php
 	include("includes/config.php");
-	
+
 	function apri_connessione()
 	{
 		include("includes/config.php");
@@ -15,12 +15,23 @@
 		$date_time = date(DATE_RFC822, $timestamp);
 		return $date_time;
 	}
-		
+
 	$operator = "1";
 	$current_timestamp = time();
-	
+
 	$link = @mysql_connect($db_host,$db_user,$db_password) or die('Unable to establish a DB connection');
 
 	mysql_set_charset('utf8');
 	mysql_select_db($db_name,$link);
+
+	function generateRandomString($length = 10) {
+	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+	    }
+	    return $randomString;
+	}
+
+
 ?>
